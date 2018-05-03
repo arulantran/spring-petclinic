fddae91d2b7a9dc1d665ceeac7def4d87497559a
Arul Antran Vijay S <arulantrans@gmail.com>
2018-05-03 16:49:34 +0530

    Update PetClinicApplication.java

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -31,10 +31,5 @@ public class PetClinicApplication {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(PetClinicApplication.class, args);
     }
-    @Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		
-		return super.configure(builder);
-}
 
 }
